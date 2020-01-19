f184363442b0a5582682ad144af6b2d61155405a
Ramin Sedighi <ramin@Ramins-MacBook-Pro.local>
2020-01-19 01:23:49 -0800

    customfield 2 added

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
 				"Content-Type": "application/json",
 				"apikey": "e8DE41C4-6167-43BC-BE78-19E522DB8668"
 			},
-			"data": JSON.stringify({"Id":"","UserName":"(payload.data.email)","FirstName":"(payload.data.firstname)","LastName":"","lastname":"","Email":"payload.data.email","AccessLevel":"Learner","DisableMessages":"false","Active":"true","Skype":"","PhoneWork":"","PhoneMobile":"","IsCustomUsername":"true","SkipFirstLogin":"true","TimeZone":"Pacific Standard Time","Street1":"","Street2":"","City":"","State":"","PostalCode":"","Country":"","CompanyName":"","JobTitle":"","CustomField1":"payload.data.partner-code","CustomField2":"","CustomField3":"","CustomField4":"","CustomField5":"","CustomField6":"","CustomField7":"","CustomField8":"","CustomField9":"","CustomField10":"","EnableTextNotification":"false"}),
+			"data": JSON.stringify({"Id":"","UserName":"(payload.data.email)","FirstName":"(payload.data.firstname)","LastName":"","lastname":"","Email":"payload.data.email","AccessLevel":"Learner","DisableMessages":"false","Active":"true","Skype":"","PhoneWork":"","PhoneMobile":"","IsCustomUsername":"true","SkipFirstLogin":"true","TimeZone":"Pacific Standard Time","Street1":"","Street2":"","City":"","State":"","PostalCode":"","Country":"","CompanyName":"","JobTitle":"","CustomField1":"payload.data.partner-code","CustomField2":"payload.data.region","CustomField3":"","CustomField4":"","CustomField5":"","CustomField6":"","CustomField7":"","CustomField8":"","CustomField9":"","CustomField10":"","EnableTextNotification":"false"}),
 		};
 
 		$.ajax(settings).done(function (response) {
